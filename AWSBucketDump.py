ec61b2859001e52f5999314ce35fc9d108e4c47b
Ryan Basden <rpbasden@gmail.com>
2018-01-09 22:46:50 -0500

    Even further output clarification, looking into adding option to save ot specific directory when downloading from instance.

--- AWSBucketDump.py
+++ AWSBucketDump.py
@@ -183,7 +183,7 @@ def main():
     parser.add_argument("-l", dest="hostlist", required=True, help="") 
     parser.add_argument("-g", dest="grepwords", required=False, help="Provide a wordlist to grep for.")
     parser.add_argument("-m", dest="maxsize", type=int, required=False, default=1024, help="Maximum file size to download.")
-    parser.add_argument("-t", dest="threads", type=int, required=False, default=1, help="Thread count.")
+    parser.add_argument("-t", dest="threads", type=int, required=False, default=1, help="Number of threads.")
 
     if len(sys.argv) == 1:
         print_banner()
